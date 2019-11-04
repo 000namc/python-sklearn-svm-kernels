c7efbca060b9b5b0e4775592f3787327cee083e8
000namc <000namc@gmail.com>
2019-11-04 09:48:12 +0900

    layout updates

--- evaluate.py
+++ evaluate.py
@@ -1,19 +1,22 @@
 import os
 import sys
 import json
+import imp
 import pandas as pd
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.svm import SVR
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
 from kernels import *
 data_name = sys.argv[1]
-data = pd.readdata = pd.read_csv(data_name + '.csv')
+data = pd.readdata = pd.read_csv('data/processed/' + data_name + '.csv')
 
 with open('names.json', mode = 'r') as io:
     names = json.loads(io.read())
-cols = names[data_name]
+cols = names[data_name]['cols']
 target = ['target']
 
 kernel_names = ['linear'
@@ -25,7 +28,6 @@ kernel_names = ['linear'
                ,'inverse_multiquadratic'
                ,'log'
                ,'power']
-
 
 score_train = {}
 score_valid = {}
@@ -46,7 +48,12 @@ for kernel_name in kernel_names:
         valid_y = data.loc[valid_index,'target'].copy()
 
         kernel_ = getattr(sys.modules[__name__], "%s_kernel" % kernel_name)
-        model = SVC(kernel = 'precomputed', cache_size = 30000)
+        
+        if names[data_name]['type'] == 'classification':
+            model = SVC(kernel = 'precomputed', cache_size = 30000)
+        if names[data_name]['type'] == 'regression':
+            model = SVR(kernel = 'precomputed', cache_size = 30000)
+        
         X = np.array(train_x[cols])
         gram_matrix = kernel_(X,X)
         model.fit(gram_matrix,train_y)
@@ -56,9 +63,13 @@ for kernel_name in kernel_names:
         X_valid = np.array(valid_x[cols])
         gram_matrix_valid = kernel_(X_valid,X)
         preds_valid = model.predict(gram_matrix_valid)
-
-        score_train[kernel_name][count] = accuracy_score(preds_train,train_y)
-        score_valid[kernel_name][count] = accuracy_score(preds_valid,valid_y)
+        
+        if names[data_name]['type'] == 'classification':
+            score_train[kernel_name][count] = accuracy_score(preds_train,train_y)
+            score_valid[kernel_name][count] = accuracy_score(preds_valid,valid_y)
+        if names[data_name]['type'] == 'regression':
+            score_train[kernel_name][count] = r2_score(preds_train,train_y)
+            score_valid[kernel_name][count] = r2_score(preds_valid,valid_y)
 
 #         print(count, '번째 fold의 결과')
 #         print(score_train[kernel_name][count])
@@ -78,5 +89,6 @@ result = pd.DataFrame()
 result['kernel'] = kernel_names
 result['train_score'] = [np.array(score_train[kernel_name]).mean() for kernel_name in kernel_names]
 result['valid_score'] = [np.array(score_valid[kernel_name]).mean() for kernel_name in kernel_names]
+result['valid_std'] = [np.array(score_valid[kernel_name]).std() for kernel_name in kernel_names]
 
-result.to_csv(data_name + '_result.csv', index = False)
+result.to_csv('result/' + data_name + '.csv', index = False)
